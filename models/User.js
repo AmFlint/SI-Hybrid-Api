a4f16fc62803ce21893446d12bcd3c4d094cc008
AmFlint <amasselot@hotmail.com>
2018-01-16 15:13:44 +0100

    add validation rules for User model

--- models/User.js
+++ models/User.js
@@ -5,25 +5,45 @@ const User = sequelize.define('users', {
   email: {
     type: Sequelize.STRING(50),
     unique: 'compositeIndex',
+    allowNull: false,
     validate: {
       isEmail: {
         msg: 'Email field must be formatted as an e-mail adress'
       },
       len: {
-        args: [[4, 50]],
-
+        args: [4, 50],
+        msg: 'Email field must no be less than 4 characters and more than 50.'
+      },
+      isUnique: function (email) {
+        User.find({ where: {email} })
+          .then(function (u) { // This gets called
+            if(u){
+              throw new Error({error:[{message:'Email address already in use!'}]});
+            }
+        });
       }
     }
   },
   username: {
     type: Sequelize.STRING(30),
     unique: 'compositeIndex',
+    allowNull: false,
     validate: {
-
+      len: {
+        args: [4, 30],
+        msg: 'Username length must be at least 4 characters and at most 30.'
+      }
     }
   },
   password: {
-    type: Sequelize.STRING(150)
+    type: Sequelize.STRING(150),
+    allowNull: false,
+    validate: {
+      len: {
+        args: [6, 150],
+        msg: 'Password length must be between 6 characters and 150'
+      }
+    }
   }
 });
 

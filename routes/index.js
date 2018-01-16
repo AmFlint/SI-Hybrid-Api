fba025712308afac0b51500efba645aece36cb94
AmFlint <amasselot@hotmail.com>
2018-01-16 13:09:51 +0100

    add User tables

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+const sequelize = require('../config/db');
+const User = require('../models/User');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('hello');
+  User.findAll().then(users => {
+    res.send(users);
+  });
 });
 
 module.exports = router;

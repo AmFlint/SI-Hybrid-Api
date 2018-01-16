a69df006dd87998ff8d8065cbb00a8a4f672adef
AmFlint <amasselot@hotmail.com>
2018-01-16 14:25:21 +0100

    fix docker-compose for dev and Synchronization Users table

--- models/User.js
+++ models/User.js
@@ -3,24 +3,31 @@ const sequelize = require('../config/db');
 
 const User = sequelize.define('users', {
   email: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING(50),
+    unique: 'compositeIndex',
+    validate: {
+      isEmail: {
+        msg: 'Email field must be formatted as an e-mail adress'
+      },
+      len: {
+        args: [[4, 50]],
+
+      }
+    }
   },
   username: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING(30),
+    unique: 'compositeIndex',
+    validate: {
+
+    }
   },
   password: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING(150)
   }
 });
 
 // force: true will drop the table if it already exists
-User.sync({force: true}).then(() => {
-  // Table created
-  return User.create({
-    email: 'john@doe.fr',
-    username: 'Hancock',
-    password: 'test'
-  });
-});
+User.sync();
 
 module.exports = User;

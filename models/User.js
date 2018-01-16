23cabd50d65f519eafb835de0c9be810892e9edb
AmFlint <amasselot@hotmail.com>
2018-01-16 12:00:43 +0100

    create base User Model for database

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../config/db');
+
+const User = sequelize.define('users', {
+  email: {
+    type: Sequelize.STRING
+  },
+  username: {
+    type: Sequelize.STRING
+  },
+  password: {
+    type: Sequelize.STRING
+  }
+});
+
+// force: true will drop the table if it already exists
+// User.sync({force: true}).then(() => {
+//   // Table created
+//   return User.create({
+//     email: 'john@doe.fr',
+//     username: 'Hancock',
+//     password: 'test'
+//   });
+// });
+
+module.exports = User;
